40d1bc5428b7cec66737b3770c49e5d3e542da72
Raphael Bonfim <raphael.bonfim.fer@gmail.com>
2023-06-14 22:59:22 -0300

    Small adjustments .

--- Program.cs
+++ Program.cs
@@ -17,7 +17,7 @@ class Program
 
         string data = Console.ReadLine()!.ToLower();
         char type = char.Parse(data.Substring(data.Length - 1, 1));
-        int time = int.Parse(data.Substring(0, data.Length - 1));da
+        int time = int.Parse(data.Substring(0, data.Length - 1));
         int multiplier = 1;
 
         if (type == 'm')
