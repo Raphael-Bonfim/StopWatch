67cbd568833a012bfe6a4b04e31ff62efbafe3ff
Raphael Bonfim <raphael.bonfim.fer@gmail.com>
2023-06-14 22:46:37 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿namespace StopWatch;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Menu();
+    }
+
+
+    
+    static void Menu()
+    {
+        Console.Clear();
+        Console.WriteLine("S = Seconds => 10s = 10 seconds");
+        Console.WriteLine("M = Minute => 1m = 1 Minute");
+        Console.WriteLine("0 = Exit");
+        Console.WriteLine("How much time do you want to count?");
+
+        string data = Console.ReadLine()!.ToLower();
+        char type = char.Parse(data.Substring(data.Length - 1, 1));
+        int time = int.Parse(data.Substring(0, data.Length - 1));
+        int multiplier = 1;
+
+        if (type == 'm')
+        {
+            multiplier = 60;
+        }
+
+        if (time == 0)
+        {
+            System.Environment.Exit(0);
+        }
+
+        PreStart(time * multiplier);
+    }
+
+    static void PreStart(int time)
+    {
+        Console.Clear();
+        Console.WriteLine("Ready...");
+        Thread.Sleep(1000);
+        Console.WriteLine("Set...");
+        Thread.Sleep(1000);
+        Console.WriteLine("Go!");
+        Thread.Sleep(2500);
+
+        Start(time);
+    }
+
+    static void Start(int time)
+    {      
+        int currentTime = 0;
+
+        while (currentTime != time)
+        {
+            Console.Clear();
+            currentTime++;
+
+            Console.WriteLine(currentTime.ToString());
+
+            Thread.Sleep(1000);
+        }
+
+        Console.Clear();
+        Console.WriteLine("StopWatch finished.");
+        Thread.Sleep(2500);
+    }
+
+    static void ExibirTituloDaOpcao(string data)
+    {
+        int quantidadeDeLetras = data.Length;
+        string traco = string.Empty.PadLeft(quantidadeDeLetras, '-');
+        Console.WriteLine(traco);
+        Console.WriteLine(data);
+        Console.WriteLine(traco + "\n");
+    }
+}
